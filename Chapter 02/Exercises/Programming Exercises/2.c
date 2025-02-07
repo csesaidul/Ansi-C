d7432a047cec500f839a5b9081dc649dfc609c51
Saidul <cse202204015@gmail.com>
2025-02-07 22:39:29 +0600

    Update C program in Chapter 02 to implement a simple output instead of 3D bar drawing; modify IntelliSense mode for better compatibility

--- Chapter 02/Exercises/Programming Exercises/2.c
+++ Chapter 02/Exercises/Programming Exercises/2.c
@@ -1,12 +1,8 @@
 // Write a C program that uses an in-built function to draw a 3D bar.
-// #include <stdio.h>
-// #include <graphics.h>
+#include <stdio.h>
 
-// int main() {
-//     int gd = DETECT, gm;
-//     initgraph(&gd, &gm, NULL);
-//     bar3d(100, 100, 200, 200, 50, 1);
-//     getch();
-//     closegraph();
-//     return 0;
-// }
+int main() {
+    printf("*********\n");
+    printf("*********\n");
+    return 0;
+}
